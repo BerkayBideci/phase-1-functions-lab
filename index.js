200b7e0e8095347164df240f13910f66e470211f
Berkay Bideci <berkaybideci@gmail.com>
2023-03-31 23:35:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+
+const headquarters = 42;
+
+function distanceFromHqInBlocks(block) {
+  if (headquarters > block) {
+    return headquarters - block;
+  }
+  if (block > headquarters) {
+    return block - headquarters;
+  }
+}
+
+function distanceFromHqInFeet(blockInFeet) {
+  return distanceFromHqInBlocks(blockInFeet) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (start > destination) {
+    return (start - destination) * 264;
+  }
+  if (destination > start) {
+    return (destination - start) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  if (distanceTravelledInFeet(start, destination) < 400) {
+    return 0;
+  }
+  if (
+    distanceTravelledInFeet(start, destination) > 400 &&
+    distanceTravelledInFeet(start, destination) < 2000
+  ) {
+    return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+  }
+  if (
+    distanceTravelledInFeet(start, destination) > 2000 &&
+    distanceTravelledInFeet(start, destination) < 2500
+  ) {
+    return 25;
+  }
+  if (distanceTravelledInFeet(start, destination) > 2500) {
+    return "cannot travel that far";
+  }
+}
